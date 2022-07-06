762edc8257ce5faa835de0bd3f45a3f70355396b
qingyun <qingyunbaofu@outlook.com>
2022-07-06 14:04:02 +0800

    逐个引入iconsâ

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <router-view />
   </div>
 </template>
+<script lang="ts">
 
+export default{
+  name:"App"
+}
+</script>
 <style lang="scss">
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,35 @@
 <template>
     <div class="nav">
         <div>
-            <router-link to="/money">记账</router-link>|
-            <router-link to="/label">标签</router-link>|
-            <router-link to="/statistics">统计</router-link>
+            <router-link to="/money">
+              <svg>
+                <use xlink:href="#money"></use>
+              </svg>
+              记账
+            </router-link>|
+            <router-link to="/label">
+              <svg>
+                <use xlink:href="#label"></use>
+              </svg>
+              标签
+            </router-link>|
+            <router-link to="/statistics">
+              <svg>
+                <use xlink:href="#statistics"></use>
+              </svg>
+              统计
+            </router-link>
         </div>
     </div>
 </template>
 
  <script lang="ts">
+ import x from '@/assets/icons/label.svg'
+ import y from '@/assets/icons/money.svg'
+ import z from '@/assets/icons/statistics.svg'
+ console.log(x)
+ console.log(y)
+ console.log(z)
 export default {
     name: 'Nav'
 }
