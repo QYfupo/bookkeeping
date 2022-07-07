4dd8170bd1b3e9d30a77b685d2d5184236f3e1c5
qingyun <qingyunbaofu@outlook.com>
2022-07-07 12:07:15 +0800

    用js实现types切换

--- src/components/Money/types.vue
+++ src/components/Money/types.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'"
+      @click="selectedType('-')">支出</li>
+      <li :class="type === '+' && 'selected'"
+          @click="selectedType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-  name: "type"
+  name: "type",
+  data(){
+    return{
+      type:'-',//'-'代表支出，'+'代表收入
+    }
+  },
+  methods:{
+    selectedType(type){
+      if(type !== '-' && type !== '+'){
+        throw new Error('type is unknown')
+      }else{
+        this.type = type
+      }
+    }
+  }
+
 }
 </script>
 
